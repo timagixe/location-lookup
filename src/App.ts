a2c70b50316be513314b9c86f4f3c1ce1240e85a
Tima Gixe <timagixe@protonmail.com>
2020-09-29 15:26:47 +0300

    Add location lookup feature

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,41 @@
-console.log('Hello');
+import axios from 'axios';
+import GoogleGeocodingResponse from './types/GoogleGeocodingResponse';
+
+const API_KEY = process.env.API_KEY;
+const formElement = document.querySelector('form') as HTMLFormElement;
+const addressInput = document.getElementById('address') as HTMLInputElement;
+const googleMapsScript = document.createElement('script');
+
+googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+document.head.append(googleMapsScript);
+
+function addressLookUpHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`)
+    .then((response) => {
+      let {
+        data: { status, results },
+      } = response as GoogleGeocodingResponse;
+      if (status !== 'OK') {
+        throw new Error('Could not fetch data from API');
+      }
+      if (results) {
+        const coordinates = results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+          center: coordinates,
+          zoom: 12,
+        });
+        new google.maps.Marker({ position: coordinates, map: map });
+      }
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+}
+
+// @ts-ignore
+const formListener = formElement.addEventListener('submit', addressLookUpHandler);
